fe05cd5777181b63ff8fc1c0a6dbcc555fb0bf5e
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-06-15 01:56:20 +0100

    gpio.s update

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,28 +1,42 @@
-ldr r0, =0x20200000 ; base GPIO Address
-mov r2, #1          ; r2 = 1
-lsl r2, #18         ; r2 = 0x20000
-str r2, [r0, #4]    ; set Pin 16 as output
+ldr r13, =0x10000             ; init stack pointer
 
-mov r1, #1
-lsl r1, #16
+ldr r4, =0x20200000           ; load base GPIO Address
+ldr r5, =0x1240               ; bits to set 02, 03, 04 as outputs
+str r5, [r4]                  ; set pins 02, 03, 04 as OUTPUT and pin 09 to INPUT
 
-mov r2, #1          ; isON = true
+mov r5, #0x1c                 ; bits [2..4] = 1
+mov r0, #1                    ; previous state = ON
 
-loop:
+mov r6, #0                    ; count = 0 (lsl 2, to align bits)
+counterLoop:                  ; begin counter loop
 
-cmp r2, #0          ; if !isON
-bne else
-str r1, [r0, #28]   ; set Pin 16
-b endif
-else:
-str r1, [r0, #40]   ; clear Pin 16
-endif:
-eor r2, r2, #1      ; toggle isON
+str r5, [r4, #0x28]           ; clear all pins
 
-mov r3, #0x2e0000   ; set delay = 0x2e0000
-delay:              ; begin delay
-sub r3, r3, #1
-cmp r3, #0
-bne delay           ; end delay
+str r6, [r4, #0x1c]           ; set pins
 
-b loop              ; and repeat
+add r6, r6, #4                ; count++ (lsl 2, to align bits)
+
+; call waitForObstruction
+add r14, r15, #8              ; r14 = PC + 8 (just after branch)
+sub r13, r13, #4              ; SP = SP - 4
+str r14, [r13]                ; push return address
+b fn_waitForObstruction       ; call delay
+
+cmp r6, #32                   ; compare counter
+blt counterLoop               ; if >= 32 break
+
+andeq r0, r0, r0              ; halt
+
+fn_waitForObstruction:
+
+ldr r1, [r4, #0x34]           ; get inputs from pins
+mov r1, r1, lsr #9            ; shift bit 9 down to bit 0
+and r1, r1, #1                ; r1 = current state
+eor r1, r1, #1                ; r1 = !r1
+tst r1, r0                    ; !current state && previous state
+eor r0, r1, #1                ; previous state = current state
+bne fn_waitForObstruction     ; return to count if new obstruction
+
+ldr r14, [r13]                ; pop return address
+add r13, r13, #4              ; SP = SP + 4
+mov r15, r14                  ; return
