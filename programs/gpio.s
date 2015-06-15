3475c4bf658db23d1c3b1b56aee2381855d04cd6
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-06-15 13:49:08 +0100

    gpio.s
    Delayed 3 LED on/off with input

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,42 +1,40 @@
-ldr r13, =0x10000             ; init stack pointer
+ldr r13, =0x10000   ; init stack pointer
+ldr r4, =0x20200000 ; load base GPIO Address
+ldr r5, =0x1240     ; bits to set 02, 03, 04 as outputs
+str r5, [r4]        ; set pins 02, 03, 04 as OUTPUT
+                    ; set pin 09 as INPUT
 
-ldr r4, =0x20200000           ; load base GPIO Address
-ldr r5, =0x1240               ; bits to set 02, 03, 04 as outputs
-str r5, [r4]                  ; set pins 02, 03, 04 as OUTPUT and pin 09 to INPUT
+mov r5, #0x1c       ; bits [2..4] = 1
 
-mov r5, #0x1c                 ; bits [2..4] = 1
-mov r0, #1                    ; previous state = ON
+loop:               ; will keep repeating
 
-mov r6, #0                    ; count = 0 (lsl 2, to align bits)
-counterLoop:                  ; begin counter loop
+str r5, [r4, #0x28] ; clear all pins
 
-str r5, [r4, #0x28]           ; clear all pins
+; call flasher
+add r14, r15, #8    ; r14 = PC + 8 (just after branch)
+sub r13, r13, #4    ; SP = SP - 4
+str r14, [r13]      ; push return address
+b fn_flasher        ; call flasher
 
-str r6, [r4, #0x1c]           ; set pins
+ldr r0, =0xfff000   ; set delay = 0xfff000
+delay:              ; begin delay
+sub r0, r0, #1
+cmp r0, #0
+bne delay           ; end delay
 
-add r6, r6, #4                ; count++ (lsl 2, to align bits)
+b loop              ; back to loop
 
-; call waitForObstruction
-add r14, r15, #8              ; r14 = PC + 8 (just after branch)
-sub r13, r13, #4              ; SP = SP - 4
-str r14, [r13]                ; push return address
-b fn_waitForObstruction       ; call delay
+andeq r0,r0,r0
 
-cmp r6, #32                   ; compare counter
-blt counterLoop               ; if >= 32 break
+fn_flasher:
 
-andeq r0, r0, r0              ; halt
+ldr r1, [r4, #0x34] ; r1 = input bits
+and r1, r1, #0x200  ; only bit 9 (current state)
+cmp r1, #0x200      ; check current state
+bne end             ; if on, set pins
+str r5, [r4, #0x1c] ; set pins
+end:
 
-fn_waitForObstruction:
-
-ldr r1, [r4, #0x34]           ; get inputs from pins
-mov r1, r1, lsr #9            ; shift bit 9 down to bit 0
-and r1, r1, #1                ; r1 = current state
-eor r1, r1, #1                ; r1 = !r1
-tst r1, r0                    ; !current state && previous state
-eor r0, r1, #1                ; previous state = current state
-bne fn_waitForObstruction     ; return to count if new obstruction
-
-ldr r14, [r13]                ; pop return address
-add r13, r13, #4              ; SP = SP + 4
-mov r15, r14                  ; return
+ldr r14, [r13]      ; pop return address
+add r13, r13, #4    ; SP = SP + 4
+mov r15, r14        ; return
