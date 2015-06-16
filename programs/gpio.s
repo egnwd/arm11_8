29587cf06c90a91a2b61b20a46af9e2a04831a33
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-06-16 11:20:24 +0100

    Change Delay

--- programs/gpio.s
+++ programs/gpio.s
@@ -3,11 +3,11 @@ ldr r5, =0x1240     ; bits to set 02, 03, 04 as outputs
 str r5, [r4]        ; set pins 02, 03, 04 as OUTPUT
                     ; set pin 09 as INPUT
 
-mov r7, #0x200          ; previous state is ON
+mov r7, #0x200      ; previous state is ON
 mov r5, #0x1c       ; bits [2..4] = 1
 mov r6, #0          ; count = 0 (lsl 2, to align bits)
 
-counterLoop:               ; will keep repeating
+counterLoop:        ; will keep repeating
 
 str r5, [r4, #0x28] ; clear all pins
 str r6, [r4, #0x1c] ; set pins
@@ -20,7 +20,7 @@ tst r1, r7          ; previous && !new
 eor r7, r1, #0x200  ; previous state = new state
 beq wait            ; if previous && !new break
 
-ldr r0, =0x4e0000   ; set delay = 0x4e0000
+ldr r0, =0x138000   ; set delay = 0x138000
 delay:              ; begin delay
 sub r0, r0, #1
 cmp r0, #0
@@ -29,7 +29,7 @@ bne delay           ; end delay
 add r6, r6, #4      ; count++
 
 cmp r6, #32
-blt counterLoop              ; if >= 32 break
+blt counterLoop     ; if >= 32 break
 
 andeq r0,r0,r0
 
